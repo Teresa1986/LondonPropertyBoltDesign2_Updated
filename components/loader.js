d35f725b093c4aa02cfa099fff1efad1babdd80f
Sujatha Varghese <s.joseph@tec.com.kw>
2025-05-28 15:22:28 +0300

    New images addition for offices

--- components/loader.js
+++ components/loader.js
@@ -136,6 +136,141 @@ function initializeOwlCarousel() {
     });
   }
 }
+function initializeOfficeCarousel() {
+  const images = [
+    "src/Image/Dean Bradley/Dean-02.jpg",
+    "src/Image/Dean Bradley/Dean-03.jpg",
+    "src/Image/Dean Bradley/Dean-04.jpg",
+    // Add more images if needed
+  ];
+  let currentIndex = 0;
+  const carouselImage = document.getElementById("carouselImage");
+  const prevBtn = document.getElementById("prevBtn");
+  const nextBtn = document.getElementById("nextBtn");
+  let intervalId;
+
+  function showImage(index) {
+    carouselImage.src = images[index];
+  }
+
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  }
+
+  function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+  }
+
+  prevBtn.onclick = function () {
+    prevImage();
+    resetInterval();
+  };
+  nextBtn.onclick = function () {
+    nextImage();
+    resetInterval();
+  };
+
+  function startInterval() {
+    intervalId = setInterval(nextImage, 3000); // Change image every 3 seconds
+  }
+
+  function resetInterval() {
+    clearInterval(intervalId);
+    startInterval();
+  }
+
+  showImage(currentIndex);
+  startInterval();
+}
+
+function initializeResidentialCarousels() {
+  // Chelsea Cloisters Carousel
+  const chelseaImages = [
+    "src/Image/Residential/Chelsea Cloisters/Cloisters-01.jpg",
+    "src/Image/Residential/Chelsea Cloisters/Cloisters-02.jpg",
+    "src/Image/Residential/Chelsea Cloisters/Cloisters-03.jpg",
+    // Add more images as needed
+  ];
+  let chelseaIndex = 0;
+  const chelseaImg = document.getElementById("chelseaCarouselImage");
+  const chelseaPrev = document.getElementById("chelseaPrevBtn");
+  const chelseaNext = document.getElementById("chelseaNextBtn");
+  let chelseaInterval;
+
+  function showChelseaImage(idx) {
+    chelseaImg.src = chelseaImages[idx];
+  }
+  function nextChelsea() {
+    chelseaIndex = (chelseaIndex + 1) % chelseaImages.length;
+    showChelseaImage(chelseaIndex);
+  }
+  function prevChelsea() {
+    chelseaIndex =
+      (chelseaIndex - 1 + chelseaImages.length) % chelseaImages.length;
+    showChelseaImage(chelseaIndex);
+  }
+  chelseaPrev.onclick = function () {
+    prevChelsea();
+    resetChelseaInterval();
+  };
+  chelseaNext.onclick = function () {
+    nextChelsea();
+    resetChelseaInterval();
+  };
+  function startChelseaInterval() {
+    chelseaInterval = setInterval(nextChelsea, 3000);
+  }
+  function resetChelseaInterval() {
+    clearInterval(chelseaInterval);
+    startChelseaInterval();
+  }
+  showChelseaImage(chelseaIndex);
+  startChelseaInterval();
+
+  // Empire Court Carousel
+  const empireImages = [
+    "src/Image/Residential/Empire Court/Empire-02.jpg",
+    "src/Image/Residential/Empire Court/Empire-03.jpg",
+    "src/Image/Residential/Empire Court/Empire-04.jpg",
+    // Add more images as needed
+  ];
+  let empireIndex = 0;
+  const empireImg = document.getElementById("empireCarouselImage");
+  const empirePrev = document.getElementById("empirePrevBtn");
+  const empireNext = document.getElementById("empireNextBtn");
+  let empireInterval;
+
+  function showEmpireImage(idx) {
+    empireImg.src = empireImages[idx];
+  }
+  function nextEmpire() {
+    empireIndex = (empireIndex + 1) % empireImages.length;
+    showEmpireImage(empireIndex);
+  }
+  function prevEmpire() {
+    empireIndex = (empireIndex - 1 + empireImages.length) % empireImages.length;
+    showEmpireImage(empireIndex);
+  }
+  empirePrev.onclick = function () {
+    prevEmpire();
+    resetEmpireInterval();
+  };
+  empireNext.onclick = function () {
+    nextEmpire();
+    resetEmpireInterval();
+  };
+  function startEmpireInterval() {
+    empireInterval = setInterval(nextEmpire, 3000);
+  }
+  function resetEmpireInterval() {
+    clearInterval(empireInterval);
+    startEmpireInterval();
+  }
+  showEmpireImage(empireIndex);
+  startEmpireInterval();
+}
 
 // Handle team-photo image fallback
 document.addEventListener("DOMContentLoaded", function () {
@@ -160,4 +295,9 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", () => {
   loadComponents();
   highlightActiveNavLink();
+
+  setTimeout(() => {
+    initializeOfficeCarousel();
+    initializeResidentialCarousels();
+  }, 300);
 });
